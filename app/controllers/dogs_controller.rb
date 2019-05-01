1b1a6ec928426ffd7d5cb70e5b9b8378861324d0
ryanden2018 <denlinger.ryan@gmail.com>
2019-05-01 14:23:42 -0500

    done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,10 @@
 class DogsController < ApplicationController
+    def index
+        @dogs = Dog.all.sort_by { |dog| dog.employees.uniq.count }
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+        @employees = @dog.employees.uniq
+    end
 end
