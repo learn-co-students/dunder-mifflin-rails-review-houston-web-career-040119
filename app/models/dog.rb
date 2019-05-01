07db586359fb26d76350ceae7bbec4af1903e1c5
github username <github email address>
2019-05-01 14:57:19 -0500

    Done.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,11 @@
 class Dog < ApplicationRecord
+    has_many :employees
+
+    def self.sort_by_employees
+        dogs = self.all
+
+        dogs.sort_by do |dog|
+            dog.employees.count
+        end
+    end
 end
